9b392982832bd244822d75c2a81bc006c07c547f
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2025-04-10 16:49:16 +0100

    added initial configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.android.volley:volley:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
