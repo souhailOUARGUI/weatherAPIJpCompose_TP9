4aa96c7f46657e6db776a5196d26f2e773b56da7
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2025-04-10 17:24:10 +0100

    Mise à jour de MainActivity pour utiliser les composants Compose"

--- app/src/main/java/com/example/weatherapijpcompose_tp9/MainActivity.kt
+++ app/src/main/java/com/example/weatherapijpcompose_tp9/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.weatherapijpcompose_tp9
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapijpcompose_tp9.ui.MeteoScreen
 import com.example.weatherapijpcompose_tp9.ui.theme.WeatherAPIJpCompose_TP9Theme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Configuration StrictMode comme dans le code d'origine
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+
         setContent {
-            WeatherAPIJpCompose_TP9Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            WeatherAPIJpCompose_TP9Theme  {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MeteoScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAPIJpCompose_TP9Theme {
-        Greeting("Android")
     }
 }
